bb30d1c6f11f9462c6146944a4d0acda401656be
paul <paul>
2024-12-09 16:32:14 +0100

    NOW WITH FUNCTIONNING BUTTON

--- script.js
+++ script.js
@@ -3,23 +3,40 @@ function addMusicDisc(){
 
     // Create a new music-disc-input-div element
     const newMusicDisc = document.createElement('div');
-    newMusicDisc.classList.add('music-disc-input-div');
+    newMusicDisc.classList.add('song');
 
     // Add inner HTML for the new music-disc-input-div
     newMusicDisc.innerHTML = `
-        <div class="input-group">
-            <label for="name">Enter Name:</label>
-            <input type="text" id="name" name="name">
-        </div>
-        <div class="input-group">
-            <label for="author">Enter Author Name:</label>
-            <input type="text" id="author" name="author">
-        </div>
-        <div class="input-group">
-            <input type="file" class="file-input" name="file" accept="audio/ogg" style="display: none;">
-            <label class="upload-label">Upload File</label>
-        </div>
-        <button class="remove-button" onclick="removeMusicDisc(this)">Remove</button>
+        <li class = "song-image-li song-item">
+            <div class = "song-image-container">
+                <img src="dummy" class="song-image" alt="Disc Texture">
+            </div>
+        </li>
+        <li class = "song-title-li song-item">
+            <div class="song-title">
+                Title
+            </div>
+        </li>
+        <li class = "song-author-li song-item">
+            <div class="song-author">
+                Author
+            </div>
+        </li>
+        <li class = "song-file-li song-item">
+            <div class="song-file">
+                File
+            </div>
+        </li>
+        <li class = "song-length-li song-item">
+            <div class="song-length">
+                Time
+            </div>
+        </li>
+        <li class = "song-remove song-item">
+            <div id="removeSong" class="remove-song-button ">
+                <span class = "cross-sign" onclick="addMusicDisc()">⨉</span>
+            </div>
+        </li>
     `;
 
 
